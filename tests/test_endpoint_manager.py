82ad1a0ab579076aee4f22abceddb2c02629b555
jurjenwerkaccount <jurjen.v.genugten@gmail.com>
2022-09-04 15:35:42 +0200

    Debugging

--- tests/test_endpoint_manager.py
+++ tests/test_endpoint_manager.py
@@ -10,9 +10,9 @@ def test_index(client, auth):
     auth.login()
     response = client.get('/')
     assert b'Log Out' in response.data
-    assert b'test title' in response.data
+    assert b'test name' in response.data
     assert b'by test on 2018-01-01' in response.data
-    assert b'test\nbody' in response.data
+    assert b'test\ndata' in response.data
     assert b'href="/1/update"' in response.data
 
 @pytest.mark.parametrize('path', (
@@ -47,15 +47,6 @@ def test_author_required(app, client, auth):
 def test_exists_required(client, auth, path):
     auth.login()
     assert client.post(path).status_code == 404
-
-@pytest.mark.parametrize('path', (
-    '/create',
-    '/1/update',
-    '/1/delete',
-))
-def test_login_required(client, path):
-    response = client.post(path)
-    assert response.headers["Location"] == "/auth/login"
 
 
 def test_author_required(app, client, auth):
